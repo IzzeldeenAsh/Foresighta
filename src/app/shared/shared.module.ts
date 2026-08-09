64a8d41c1e26ee3af5716efa197d5406d8131481
Izzeldeen Ashour <112341444+IzzeldeenAsh@users.noreply.github.com>
2026-08-09 12:16:51 +0300

    Update

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -9,14 +9,12 @@ import { CheckboxModule } from 'primeng/checkbox';
 import { ButtonModule } from 'primeng/button';
 import { EmptyStateComponent } from './components/empty-state/empty-state.component';
 import { OtpModalComponent } from '../reusable-components/otp-modal/otp-modal.component';
-import { CountryUpdateModalComponent } from '../reusable-components/country-update-modal/country-update-modal.component';
 import { CustomYearPickerComponent } from '../reusable-components/custom-year-picker/custom-year-picker.component';
 
 @NgModule({
   declarations: [
     EmptyStateComponent,
     OtpModalComponent,
-    CountryUpdateModalComponent,
     CustomYearPickerComponent
   ],
   imports: [
@@ -33,7 +31,6 @@ import { CustomYearPickerComponent } from '../reusable-components/custom-year-pi
   exports: [
     EmptyStateComponent,
     OtpModalComponent,
-    CountryUpdateModalComponent,
     CustomYearPickerComponent
   ]
 })
